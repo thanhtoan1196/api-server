0e274507d728ba262dcbdee6a14b10673a1a62ba
Khoa Nguyen <khoa.nguyen@codecomplete.jp>
2017-12-06 17:08:18 +0700

    Final fixing issue

--- web-server-api/post.js
+++ web-server-api/post.js
@@ -16,18 +16,27 @@ if (system.args.length === 1) {
         console.log('Unable to post!');
     } else {
         var title = 'undefined';
-		title = page.evaluate(function() {
-			// return document.documentElement.innerHTML;
-			return document.getElementsByTagName("script")[5].innerHTML;
-		});
-        // title = 'undefined';
-        if (isEmpty(title)) {
-            console.log('[]');
-        } else if (title.indexOf("undefined") >= 0) {
-            console.log(title);
+        title = page.evaluate(function() {
+            // return document.documentElement.innerHTML;
+            return document.getElementsByTagName("iframe")[0].getAttribute("src");
+        });
+        if (!isEmpty(title)) {
+            var data = '[ {"file": "view.php?v=CxcXExBZTEwQFwwRAgQGTQQMDAQPBgITChBNAAwOTAYWEQwTBk4UBhAXUlNNAhMTEBMMF00ADA5MCwwATCACEwZNJQYCEU1SWlVRTVRRUxNNIQ8WMQIaTStRVVdNIiIgTjEiMSEkTQ4TVw==","type": "mp4", "label": "Link was died"}]';
+            console.log(data);
         } else {
-    		var content = title.split("\n");
-            console.log(content[3].replace("view.php", "http://m4ufree.com/view.php"));
+    		title = page.evaluate(function() {
+    			// return document.documentElement.innerHTML;
+    			return document.getElementsByTagName("script")[5].innerHTML;
+    		});
+            // title = 'undefined';
+            if (isEmpty(title)) {
+                console.log('undefined');
+            } else if (title.indexOf("undefined") >= 0) {
+                console.log(title);
+            } else {
+        		var content = title.split("\n");
+                console.log(content[3].replace("view.php", "http://m4ufree.com/view.php"));
+            }
         }
     }
     phantom.exit();
