c1877aee77ddd18bb8e89b4be12fe01c082d2d0e
vankhoa01 <vankhoa0401@gmail.com>
2018-06-15 17:07:22 +0700

    Update getWatchLink.php

--- web-server-api/getWatchLink.php
+++ web-server-api/getWatchLink.php
@@ -5,11 +5,20 @@ $title = $_GET['title'];
 $typesub = $_GET['typesub'];
 $sub = $_GET['sub'];
 $cover = $_GET['cover'];
+$efg = $_GET['efg'];
+$rl = $_GET['rl'];
+$vabr = $_GET['vabr'];
+$oh = $_GET['oh'];
+$oe = $_GET['oe'];
 if($video == ''){
 	echo "Where is the media ID?"; //https://openload.co/embed/7zLUwKrlQqCk  (The ID is "7zLUwKrlQqCk" in this case)
 	exit();
 }else{
 	// $response = str_replace('https://', 'http://', $video);
-	echo $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
+	if (strpos($video, 'fbcdn') !== false) {
+    	     echo $video . "&efg=" . $efg . "&rl=" . $rl . "&vabr=" . $vabr . "&oh=" . $oh . "&oe=" . $oe;
+	} else {
+	     echo $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
+	}
 }
 ?>
