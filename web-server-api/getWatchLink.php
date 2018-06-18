e1c8368b1de3153d96847b56e2327b1236b6804f
vankhoa01 <vankhoa0401@gmail.com>
2018-06-18 17:14:17 +0700

    Update getWatchLink.php

--- web-server-api/getWatchLink.php
+++ web-server-api/getWatchLink.php
@@ -10,15 +10,48 @@ $rl = $_GET['rl'];
 $vabr = $_GET['vabr'];
 $oh = $_GET['oh'];
 $oe = $_GET['oe'];
-if($video == ''){
+if($video == '') {
 	echo "Where is the media ID?"; //https://openload.co/embed/7zLUwKrlQqCk  (The ID is "7zLUwKrlQqCk" in this case)
 	exit();
-}else{
+} else {
 	// $response = str_replace('https://', 'http://', $video);
 	if (strpos($video, 'fbcdn') !== false) {
-    	     echo $video . "&efg=" . $efg . "&rl=" . $rl . "&vabr=" . $vabr . "&oh=" . $oh . "&oe=" . $oe;
+    	echo $video . "&efg=" . $efg . "&rl=" . $rl . "&vabr=" . $vabr . "&oh=" . $oh . "&oe=" . $oe;
 	} else {
-	     echo $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
+		$movie = $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
+		getMovies($movie);
 	}
 }
+
+header("Content-type:application/json");
+function apivn_curl($url, $body='') {
+	$ch = @curl_init();
+	$header[] = "accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
+	$header[] = "accept-language:vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,zh-CN;q=0.4,zh;q=0.3,mt;q=0.2,ar;q=0.1";
+	$header[] = "cookie:NID=132=zL1q2-YwKpCwyro9VtPSK7uENnv0-yQi80jCkoCg2TlgYl85gZLY40vhKkNWP6EhAZYjbpR6K0y9ocMSDHTTCfbaXnSRiv98PUh8cjrJ_MoeYPnZMwjE7TBqHxFMNDxR; __cfduid=d44b2e032b3c8a2ffd8ad97e873f43b771527127691; _ga=GA1.2.552850278.1527127692; _gid=GA1.2.429859876.1529310209; token=5b276ebc7f17c; tvshow=e6e6c51hv9jveu4ea75pc7l8q7";
+	$header[] = "referer:https://vidnode.net/";
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	if($body)	curl_setopt($ch, CURLOPT_POST, true); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	if($body)	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+	curl_setopt($ch, CURLOPT_ENCODING , 'gzip, deflate');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	return $response;
+}
+
+function getMovies($curl) {
+	$get = apivn_curl($curl);
+	$pieces = explode("file: '", $get);
+	$data = explode("',label", $pieces[1]);
+	echo $data[0];
+}
+
 ?>
