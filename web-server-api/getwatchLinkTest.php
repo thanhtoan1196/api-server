f9231edea588102116e041e10287453f68321ae4
vankhoa01 <vankhoa0401@gmail.com>
2018-09-15 16:39:15 +0700

    Update getwatchLinkTest.php

--- web-server-api/getwatchLinkTest.php
+++ web-server-api/getwatchLinkTest.php
@@ -39,7 +39,7 @@ function apivn_curl($url, $body='') {
 	$ch = @curl_init();
 	$header[] = "accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
 	$header[] = "accept-language:vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,zh-CN;q=0.4,zh;q=0.3,mt;q=0.2,ar;q=0.1";
-	$header[] = "cookie:NID=132=zL1q2-YwKpCwyro9VtPSK7uENnv0-yQi80jCkoCg2TlgYl85gZLY40vhKkNWP6EhAZYjbpR6K0y9ocMSDHTTCfbaXnSRiv98PUh8cjrJ_MoeYPnZMwjE7TBqHxFMNDxR; __cfduid=d44b2e032b3c8a2ffd8ad97e873f43b771527127691; _ga=GA1.2.552850278.1527127692; _gid=GA1.2.429859876.1529310209; token=5b276ebc7f17c; tvshow=e6e6c51hv9jveu4ea75pc7l8q7";
+	$header[] = "cookie:__cfduid=d328ec335b4cf1923184c874ab307f4301529074326; _ga=GA1.2.1148281043.1529074418; __gads=ID=f725f9b7eada8da2:T=1534950160:S=ALNI_MbD-qJwdyfcKV6ZSCx9Pu_448xqKw; gogoanime=a1mk88v5k0sa5t1g67ie3bfm46; token=hDt%2F1Zh9tbbGazdHs9p5ao9GcQloT4Vt3FlvsV5VWa2NwfpFWjb3y5EnQZlKm6AlIS6pWAmbQrt3zxtTW%2FZpqw%3D%3D; _gid=GA1.2.1155025335.1537002669; BB_plg=pm; __test; bbl=6; MarketGidStorage=%7B%220%22%3A%7B%22svspr%22%3A%22%22%2C%22svsds%22%3A15%2C%22TejndEEDj%22%3A%22XqtiSVzSJ%22%7D%2C%22C160923%22%3A%7B%22page%22%3A4%2C%22time%22%3A1537002817824%7D%2C%22C264950%22%3A%7B%22page%22%3A3%2C%22time%22%3A1537002688653%7D%2C%22C242770%22%3A%7B%22page%22%3A5%2C%22time%22%3A1537002817939%7D%2C%22C267237%22%3A%7B%22page%22%3A3%2C%22time%22%3A1537002817583%7D%7D";
 	$header[] = "referer:https://vidnode.net/";
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
