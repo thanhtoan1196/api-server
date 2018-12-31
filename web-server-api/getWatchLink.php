ea47d1d14e95dd6534c9f23e9bc7e6669de4f0ad
vankhoa01 <vankhoa0401@gmail.com>
2018-12-31 19:55:41 +0700

    Update getWatchLink.php

--- web-server-api/getWatchLink.php
+++ web-server-api/getWatchLink.php
@@ -31,6 +31,8 @@ if($video == '') {
 			$movie = $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
 			getMovies($movie);
 		}
+	} else if (strpos($video, 'openload') !== false) {
+		getMovies($video);
 	} else {
 		if (strpos($video, 'https://www.rapidvideo.com/e/') !== false) {
 			$video = str_replace('https://www.rapidvideo.com/e/', 'https://www.rapidvideo.com/d/', $video);
