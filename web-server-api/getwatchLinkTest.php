99d33fe6306061e66cdcaa20b61fdaca9d2090d3
vankhoa01 <vankhoa0401@gmail.com>
2019-02-25 16:44:21 +0700

    Update getwatchLinkTest.php

--- web-server-api/getwatchLinkTest.php
+++ web-server-api/getwatchLinkTest.php
@@ -18,19 +18,26 @@ if($video == '') {
 	if (strpos($video, 'fbcdn') !== false) {
 		echo $video . "&efg=" . $efg . "&rl=" . $rl . "&vabr=" . $vabr . "&oh=" . $oh . "&oe=" . $oe;
 	} else if (strpos($video, 'vidnode.net') !== false || strpos($video, 'vidcloud.icu') !== false) {
+		if (strpos($video, 'http://') !== false) {
+			$video = str_replace('http://', 'https://', $video);
+		}
 		if (strpos($video, 'load.php') !== false) {
 			$title = str_replace(' ', '+', $title);
 			$movie = $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
+// 			$movie = str_replace('load.php', 'streaming.php', $movie);
 			echo $movie;
-			// getMovies($movie);
-			// $xxx = urlencode($movie);
-			// header("Location: $playerurl?movie=$xxx");
 		} else {
 			$title = str_replace(' ', '+', $title);
 			$movie = $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
 			getMovies($movie);
 		}
+	} else if (strpos($video, 'openload') !== false) {
+		getMovies($video);
 	} else {
+		if (strpos($video, 'https://www.rapidvideo.com/e/') !== false) {
+			$video = str_replace('https://www.rapidvideo.com/e/', 'https://www.rapidvideo.com/d/', $video);
+			$video = $video . "----------" . "a-6-href-mp4|a-7-href-mp4|a-8-href-mp4|a-9-href-mp4|a-10-href-mp4|a-11-href-mp4|a-12-href-mp4|a-13-href-mp4|a-14-href-mp4";
+		}
 		echo $video;
 	}
 }
@@ -38,12 +45,9 @@ header("Content-type:application/json");
 function apivn_curl($url, $body='') {
 	$ch = @curl_init();
 	$header[] = "accept:text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8";
-	$header[] = "accept-encoding:gzip, deflate, br";
-	$header[] = "accept-language:vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5";
-	$header[] = "cookie:__cfduid=d3d781317e8aaa0188b43e092b2c0a3ef1534950163; _ga=GA1.2.1946866875.1534950335; __test; _gid=GA1.2.248072669.1537002700";
-	$header[] = "referer:https://ww5.gowatchseries.co/";
-	$header[] = "upgrade-insecure-requests:1";
-	$header[] = "user-agent:Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Safari/537.36";
+	$header[] = "accept-language:vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5,zh-CN;q=0.4,zh;q=0.3,mt;q=0.2,ar;q=0.1";
+	$header[] = "cookie:NID=132=zL1q2-YwKpCwyro9VtPSK7uENnv0-yQi80jCkoCg2TlgYl85gZLY40vhKkNWP6EhAZYjbpR6K0y9ocMSDHTTCfbaXnSRiv98PUh8cjrJ_MoeYPnZMwjE7TBqHxFMNDxR; __cfduid=d44b2e032b3c8a2ffd8ad97e873f43b771527127691; _ga=GA1.2.552850278.1527127692; _gid=GA1.2.429859876.1529310209; token=5b276ebc7f17c; tvshow=e6e6c51hv9jveu4ea75pc7l8q7";
+	$header[] = "referer:https://vidnode.net/";
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
     curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
@@ -60,39 +64,33 @@ function apivn_curl($url, $body='') {
 	curl_close($ch);
 	return $response;
 }
-function getEmbedMovies($curl) {
-	$basedomain = file_get_contents("https://domain.embed.is/"); 
-	$get = apivn_curl($curl);
-	$pieces = explode("//embed.is/result/?id=", $get);
-	$data = explode("'", $pieces[1]);
-	if(empty($data[0])) {
-		echo "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerBlazes.mp4";
-	} else {
-		echo $basedomain . '/movie/stream.php?mid=' . $data[0];
-	}
+header("Content-type:application/json");
+function apivn_curl_ol($url, $body='') {
+	$ch = @curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_USERAGENT, $_SERVER['HTTP_USER_AGENT']); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+	if($body)	curl_setopt($ch, CURLOPT_POST, true); 
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	if($body)	curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+	curl_setopt($ch, CURLOPT_ENCODING , 'gzip, deflate');
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, FALSE);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+	$response = curl_exec($ch);
+	curl_close($ch);
+	return $response;
 }
 function getMovies($curl) {
-	echo $curl;
-	$get = apivn_curl($curl);
-	echo $get;
-	$pieces = explode("file: '", $get);
-	$data = explode("',label", $pieces[1]);
-	if (strpos($data[0], 'fbcdn') !== false) {
-		echo $data[0];
+	$piecesOL = explode("openload.co/embed/", $curl);
+	$openloadURL = $piecesOL[1];
+	$getOL = apivn_curl_ol("https://getlinkaz.com/get/API/?api=Vankhoa01&id=Fb-openload&link=https://openload.co/f/" . $openloadURL);
+	if (empty($getOL[0]) || strpos($getOL[0], '.mp4') !== false) {
+		echo $curl;
 	} else {
-		$arr = explode("/", $data[0]);
-		$link = "";
-		foreach ($arr as $key => $value) {
-		    if ($key >= 7) {
-		    	$link = $link . "/" . $value;
-		    }
-		    // echo $arr;
-		}
-		if (empty($link) != false) {
-		    echo $data[0];
-		} else {
-			echo "http://linkdelivery46.micetop.us/cdn2_vidcdn_pro" . $link;
-		}
+		echo $getOL;
 	}
 }
 ?>
