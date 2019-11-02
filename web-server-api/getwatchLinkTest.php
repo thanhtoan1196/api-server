23f737e7eeae0007ff401d60af88066961ecc45d
vankhoa01 <vankhoa0401@gmail.com>
2019-11-02 13:51:57 +0700

    Update getwatchLinkTest.php

--- web-server-api/getwatchLinkTest.php
+++ web-server-api/getwatchLinkTest.php
@@ -31,9 +31,10 @@ if($video == '') {
 			$movie = $video . "&title=" . $title . "&typesub=" . $typesub . "&sub=" . $sub . "&cover=" . $cover;
 			getMovies($movie);
 		}
-	} else if (strpos($video, 'openload') !== false) {
+	} else if (strpos($video, 'openload') !== false || strpos($video, 'streamango') !== false) {
 		// getMovies($video);
-		echo $video;
+		// echo $video;
+		echo "";
 	} else {
 		if (strpos($video, 'https://www.rapidvideo.com/e/') !== false) {
 			$video = str_replace('https://www.rapidvideo.com/e/', 'https://www.rapidvideo.com/d/', $video);
